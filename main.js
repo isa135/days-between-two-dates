6a2ea9969fefb9aee4b4748034ab6c1da43fd0b2
Isa Accessor <isa135@gmail.com>
2022-01-27 09:59:05 -0300

    Primeiro Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Days Between Two Dates</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
